39d314be85519c73e2c96c2f7cf41472b889db20
shahabrar7746 <73398955+shahabrar7746@users.noreply.github.com>
2021-05-07 14:51:04 +0530

    comment added

--- Main.java
+++ Main.java
@@ -7,11 +7,12 @@ public class Main
         int temp = 0;
         int lengthofarray = sc.nextInt();
         int array[] = new int[lengthofarray];
-       
+       //This loop will be used to give values to array
        
         for(int i = 0;i<array.length;i++){
             array[i] = sc.nextInt();
         }
+		//This loop will flip our array to right 
         int secondarylocation = array.length - 1;
         for(int i = 0;i<array.length/2;i++){
             temp = array[i];
@@ -19,6 +20,7 @@ public class Main
              array[secondarylocation - i] = temp;
             
         }
+		//this loop will print all value of array
         System.out.println();
         for(int i = 0;i<array.length;i++){
             System.out.print(array[i] + " ");
